a8050d211dc9ae2b1a67d42ac4e16b003130922e
hiibro119 <108076887+hiibro119@users.noreply.github.com>
2022-06-24 19:13:09 +0530

    Update global.js
    Signed-off-by: hiibro119 <108076887+hiibro119@users.noreply.github.com>

--- global.js
+++ global.js
@@ -1,16 +1,16 @@
 //global
 
 // ⧈⧈⧈⧈⧈⧈⧈⧈⧈⧈⧈⧈⧈⧈⧈⧈⧈⧈⧈⧈⧈⧈⧈
-//▮ZIM BOT INC 2022 ®️ALL RIGHTS RESERVED
+//▮Z𝐙𝐨𝐨𝐌📲 𝐇𝐃 INC 2022 ®️ALL RIGHTS RESERVED
 //▮
-//▮FORK AND DON'T FORGET TO GIVE A STAR
+//▮______________________________________
 //▮
 //▮THIS SOFTWARE IS UNDER UZ COPYRIGHT
 //▮
 //▮REPORT ABUSE OF THIS SOFTWARE EMAIL US
-//▮reinhardtuna@mail.uk
-//▮WHATSAPP US : +44 7441 437150
-//▮YOUTUBE CHANNELL: https://youtube.com/c/DRIPSOFC
+//▮cricketzoomhdchannel@gmail.com
+//▮WHATSAPP US : +94 758 663348
+//▮YOUTUBE CHANNELL: _________________________
 //▮
 //╰▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀
 //
